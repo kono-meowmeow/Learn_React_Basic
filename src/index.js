1c4cd36121fec421bb44d2428265c6fb32ded0fc
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-09-26 23:19:46 +0900

    コンポーネント

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react'; // reactを使うには必ずimportする必要がある
 import ReactDom from 'react-dom';
+import App from './App';
 
-const App = () => {
-  return (
-    // JSX記法において、return()の中身は一つのタグで囲わないといけない
-    // とはいえ、divタグ等を無駄に使うと必要のないものがレンダリングされる
-    // そこで、React.Fragmentを使うと良い
-    // あるいは、空のタグで囲えば良い
-    <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
-    </>
-  );
-};
-
-// index.htmlのidがrootの箇所にAPPを反映させる
+// index.htmlのidがrootの箇所にAppを反映させる
+// App.jsで定義したAppコンポーネントを使う
 ReactDom.render(<App />, document.getElementById("root"));
