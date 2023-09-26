3078a2b0be341e5654fc0aa0ad73a1560ad689ae
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-09-26 23:08:39 +0900

    JSXの基本

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react'; // reactを使うには必ずimportする必要があ
 import ReactDom from 'react-dom';
 
 const App = () => {
-  return <h1>こんにちは！</h1>;
+  return (
+    // JSX記法において、return()の中身は一つのタグで囲わないといけない
+    // とはいえ、divタグ等を無駄に使うと必要のないものがレンダリングされる
+    // そこで、React.Fragmentを使うと良い
+    // あるいは、空のタグで囲えば良い
+    <>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </>
+  );
 };
 
 // index.htmlのidがrootの箇所にAPPを反映させる
